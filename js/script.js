044de1c061b7e241fe8876c282863f6e512cdd97
andreasgiannoudis <132341487+andreasgiannoudis@users.noreply.github.com>
2023-06-01 10:55:38 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ const iconUrl = `https://openweathermap.org/img/wn/`;
 
 
 const form = document.querySelector('form');
+
+//event listener to the form
+//the form is text input where the user tyes the city 
+//and option values for the forecast every X hours
 form.addEventListener('submit', function (event) {
     event.preventDefault();
     resetEverything();
@@ -12,16 +16,19 @@ form.addEventListener('submit', function (event) {
     const forecastOptions = document.querySelector('#forecastOptions');
     const forecastValue = forecastOptions.value;
 
-    const cityUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`;
-    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+    const cityApiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`;
+    const forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
 
     form.reset();
 
-
-    fetch(cityUrl)
+    //fetching the api url based on the given city
+    //taking the latitude and longitude from the city data 
+    //and fetch the api url for the current weather in the specific coord (lat and lon)
+    //if something doesn't happen as it is planned, for example writting a city that does not exist
+    //then it catches it and throws an error message
+    fetch(cityApiUrl)
         .then(response => {
             if (response.ok) {
-                containerDiv.style.display = 'block';
                 return response.json();
             }
             else {
@@ -30,23 +37,26 @@ form.addEventListener('submit', function (event) {
         })
         .then(city => {
             const cityUrlWithCoords = `https://api.openweathermap.org/data/2.5/weather?lat=${city[0].lat}&lon=${city[0].lon}&appid=${apiKey}&units=metric`;
-            return fetch(cityUrlWithCoords);
-        })
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw 'The given city does not exist! Make sure you wrote it correct!';
-            }
-        })
-        .then(city => {
-            displayWeather(city);
-            changeBackground(city);
+            fetch(cityUrlWithCoords)
+                .then(response => {
+                    if (response.ok) {
+                        containerDiv.style.display = 'block';
+                        return response.json();
+                    } else {
+                        throw 'The given city does not exist! Make sure you wrote it correct!';
+                    }
+                })
+                .then(city => {
+                    displayWeather(city);
+                    changeBackground(city);
+                })
         })
         .catch(handleError);
 
-
-    fetch(forecastUrl)
+    //fetching the forecast api url
+    //if everything is fine it responses with json
+    //if something unexpected happens then it throw an error message
+    fetch(forecastApiUrl)
         .then(response => {
             if (response.ok) {
                 forecastDiv.style.display = 'block';
@@ -72,7 +82,10 @@ const pCityPrognos = document.querySelector('#cityPrognos');
 const pWind = document.querySelector('#wind');
 const img = document.querySelector('img');
 
-
+//based on the given city (parameter city) the function shows the weather info
+//info such like temperature in Celsius degrees, wind speed, description and icon
+//Rounds the temperature to the nearest number
+//windSpeed is converting the wind speed value to a string representation with two decimal places.
 function displayWeather(city) {
 
     containerDiv.append(h2CityName, pCityPrognos, pWind, img);
@@ -96,12 +109,14 @@ function displayWeather(city) {
 
 const forecastDiv = document.querySelector('#forecast');
 //displaying the forecast and presenting it by creating div and paragraph
+//the loop stops when the desired number of forecast entries (forecastValue) is reached or when there are no more forecast entries
+//the function iterates through the forecast data and creates HTML elements for each forecast
 function displayForecast(city, forecastValue) {
 
     const cityList = city.list;
     let forecastCount = 0;
     for (let i = 1; i < forecastValue && forecastCount < forecastValue / 3; i++) {
-        
+
         const pForecast = document.createElement('p');
         const divForEveryForecast = document.createElement('div');
         let celsius = Math.round(cityList[i].main.temp);
@@ -109,11 +124,11 @@ function displayForecast(city, forecastValue) {
         weatherIcon.src = iconUrl + cityList[i].weather[0].icon + '.png';
 
         pForecast.innerText = `${cityList[i].dt_txt.slice(11, -3)} ${cityList[i].weather[0].description} ${celsius}°`;
+        pForecast.classList.add('forecastP');
         pForecast.append(weatherIcon);
 
         divForEveryForecast.append(pForecast);
         forecastDiv.append(divForEveryForecast);
-
 
 
         forecastCount++;
@@ -126,14 +141,16 @@ function displayForecast(city, forecastValue) {
 
 
 //arrays with photos about different weather conditions
-const fewClouds = ['./img/fewClouds/fewClouds.jpg', './img/fewClouds/fewClouds1.jpg', './img/fewClouds/fewClouds2.jpg'];
-const clouds = ['./img/clouds/overcast.jpg', './img/clouds/clouds.jpg'];
-const rain = ['./img/rain/rain.jpg', './img/rain/rain1.jpg', './img/rain/rain2.jpg', './img/rain/rain3.jpg'];
-const sun = ['./img/sunny/sunny.jpg', './img/sunny/sunny1.jpg', './img/sunny/sunny2.jpg', './img/sunny/sunny3.jpg'];
-const night = ['./img/night/night.jpg', './img/night/night1.jpg', './img/night/night2.jpg'];
-const thunderstorm = ['./img/thunderstorm/thunderstorm1.jpg', './img/thunderstorm/thunderstorm2.jpg'];
+const fewClouds = ['./img/fewClouds/fewClouds.jpg', './img/fewClouds/fewClouds1.jpg', './img/fewClouds/fewClouds2.jpg', './img/fewClouds/fewClouds3.jpg', './img/fewClouds/fewClouds4.jpg'];
+const clouds = ['./img/clouds/overcast.jpg', './img/clouds/clouds.jpg', './img/clouds/clouds1.jpg', './img/clouds/clouds2.jpg', './img/clouds/clouds3.jpg'];
+const rain = ['./img/rain/rain.jpg', './img/rain/rain1.jpg', './img/rain/rain2.jpg', './img/rain/rain3.jpg', './img/rain/rain4.jpg'];
+const sun = ['./img/sunny/sunny.jpg', './img/sunny/sunny1.jpg', './img/sunny/sunny2.jpg', './img/sunny/sunny3.jpg', './img/sunny/sunny4.jpg', './img/sunny/sunny5.jpg', './img/sunny/sunny6.jpg','./img/sunny/sunny7.jpg','./img/sunny/sunny8.jpg'];
+const night = ['./img/night/night.jpg', './img/night/night1.jpg', './img/night/night2.jpg', './img/night/night3.jpg'];
+const thunderstorm = ['./img/thunderstorm/thunderstorm1.jpg', './img/thunderstorm/thunderstorm2.jpg', './img/thunderstorm/thunderstorm3.jpg'];
+const mist = ['./img/misty/misty.jpg', './img/misty/misty1.jpg', './img/misty/misty2.jpg', './img/misty/misty3.jpg'];
+const snow = ['./img/snow/snow.jpg', './img/snow/snow1.jpg', './img/snow/snow2.jpg', './img/snow/snow3.jpg', './img/snow/snow4.jpg'];
 
-//it returns a random image from a given array
+//it returns a random image from a given array 
 function generateRandomBg(array) {
     return Math.floor(Math.random() * array.length);
 }
@@ -142,7 +159,7 @@ function generateRandomBg(array) {
 
 //changing the background based on the current weather of the given city (rain background if it is raining etc)
 //also changing the background based on if the time is after sunset or before sunset
-//after sunset the background image is dark
+//after sunset the background image is a random night photo
 //the photos are random from different arrays
 function changeBackground(city) {
 
@@ -152,6 +169,7 @@ function changeBackground(city) {
     const randomSunnyImg = generateRandomBg(sun);
     const randomNightImg = generateRandomBg(night);
     const randomThunderImg = generateRandomBg(thunderstorm);
+    const randomMistyImg = generateRandomBg(mist);
 
     //getting the current time of the given city from timezone
     const currentTime = new Date().toLocaleTimeString({ timeZone: city.timezone });
@@ -178,8 +196,16 @@ function changeBackground(city) {
         else if (weatherDescription === "clear sky") {
             document.body.style.backgroundImage = 'url(' + sun[randomSunnyImg] + ')';
         }
-        else if (weatherDescription === "thunderstorm"){
+        else if (weatherDescription === "thunderstorm") {
             document.body.style.backgroundImage = 'url(' + thunderstorm[randomThunderImg] + ')';
+        }
+        else if (weatherDescription === "mist")
+        {
+            document.body.style.backgroundImage = 'url(' + mist[randomMistyImg] + ')';
+        }
+        else if (weatherDescription === "snow")
+        {
+            document.body.style.backgroundImage = 'url(' + mist[randomMistyImg] + ')';
         }
     }
     else {
@@ -196,9 +222,9 @@ function changeBackground(city) {
 
 const divError = document.querySelector('#error');
 const pError = document.querySelector('#pError');
-function handleError(error) {
-    //console.log(error);
 
+function handleError(error) {
+    
     containerDiv.style.display = 'none';
     forecastDiv.style.display = 'none';
     pError.innerText = error;
@@ -214,7 +240,6 @@ function resetEverything() {
     pError.innerText = '';
     divError.style.display = 'none';
     forecastDiv.innerHTML = '';
-
 }
 
 
